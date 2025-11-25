16a55b93bad3b68939c148315ddfda650d7e6d43
aichou <abdelali.ichou@harvest.fr>
2025-11-26 00:19:55 +0100

    adding login & signup pages design and drawables

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,55 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation (libs.legacy.support.core.utils)
+    implementation (platform(libs.firebase.bom))
+    implementation (libs.play.services.auth)
+
+    implementation (libs.firebase.auth.v2107)
+    implementation (libs.firebase.ui.auth)
+    implementation (libs.firebase.database.v2005)
+    implementation (libs.firebase.ui.database)
+    implementation (libs.firebase.firestore.v2422)
+    implementation (libs.firebase.storage.v2001)
+
+    // pinview
+    // implementation(libs.pinview)
+    implementation(libs.pinview)
+
+    // alert dialog
+    implementation(libs.material.v161)
+
+    // imageSlider
+    implementation(libs.imageslideshow)
+
+    //bottom navigation bar
+    implementation(libs.library)
+    implementation(libs.superbottombar)
+    implementation(libs.chip.navigation.bar)
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:")
+
+    // Picasso
+    implementation(libs.picasso)
+
+    // google
+    implementation (libs.firebase.appcheck.safetynet.v1601)
+    implementation (libs.browser.v140)
+
+    // shimmer effect when loading elements
+    implementation(libs.shimmer)
+
+    // lottie
+    implementation(libs.lottie)
+
+    // ticket view
+    implementation(libs.zigzagview)
+
 }
---
+++ app/src/main/java/estore/istic/fr/View/MainActivity.java
@@ -0,0 +1,28 @@
+package estore.istic.fr.View;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import estore.istic.fr.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
